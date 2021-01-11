33bee9bd77d7e3410414e0fa0d6864912164a38b
Lanre039 <rajilat1000@gmail.com>
2021-01-11 21:50:31 +0100

    feature: handled exception for invalid search

--- utils/formatData.js
+++ utils/formatData.js
@@ -6,22 +6,32 @@ const formatData = (data, currency = null) => {
     currency = currency.split(",");
     const rates = data.rates;
     const result = {};
+    let invalidSearch = [];
 
     currency.forEach((field) => {
+      // SELECT VALID SEARCH
       if (Object.keys(rates).includes(field.toUpperCase())) {
         result[field.toUpperCase()] = rates[field.toUpperCase()];
+      }
+
+      // SELECT INVALID SEARCH
+      if (!Object.keys(rates).includes(field.toUpperCase())) {
+        const message = `${field} NOT listed!`;
+        invalidSearch = [...invalidSearch, message];
       }
     });
 
     format["base"] = data.base;
     format["date"] = new Date().toLocaleDateString();
     format["rates"] = result;
+    format["invalidSearch"] = invalidSearch;
     return format;
   }
 
   format["base"] = data.base;
   format["date"] = data.date;
   format["rates"] = data.rates;
+  format["invalidSearch"] = [];
   return format;
 };
 
